f1a950e15a610e852f7cf7e4bf6d53fe19237bd5
Fu Cheng <alexcheng1982@gmail.com>
2025-04-04 22:01:40 +1300

    Use QuestionAnswerAdvisor

--- src/main/java/com/javaaidev/webpageqa/chat/ChatController.java
+++ src/main/java/com/javaaidev/webpageqa/chat/ChatController.java
@@ -1,5 +1,6 @@
 package com.javaaidev.webpageqa.chat;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatController {
 
-  private final ChatService chatService;
+  private final ChatClient chatClient;
 
-  public ChatController(ChatService chatService) {
-    this.chatService = chatService;
+  public ChatController(ChatClient chatClient) {
+    this.chatClient = chatClient;
   }
 
   @PostMapping("/chat")
   public ChatResponse chat(@RequestBody ChatRequest request) {
-    return new ChatResponse(chatService.chat(request));
+    return new ChatResponse(chatClient.prompt().user(request.input()).call().content());
   }
 }
--- src/main/java/com/javaaidev/webpageqa/chat/ChatModuleConfiguration.java
+++ src/main/java/com/javaaidev/webpageqa/chat/ChatModuleConfiguration.java
@@ -1,6 +1,7 @@
 package com.javaaidev.webpageqa.chat;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class ChatModuleConfiguration {
 
   @Bean
-  public ChatClient chatClient(ChatClient.Builder builder) {
-    return builder.build();
-  }
-
-  @Bean
-  public ChatService chatService(ChatClient chatClient,
+  public ChatClient chatClient(ChatClient.Builder builder,
       VectorStore vectorStore) {
-    return new ChatService(chatClient, vectorStore);
+    return builder.defaultAdvisors(new QuestionAnswerAdvisor(vectorStore)).build();
   }
 }
